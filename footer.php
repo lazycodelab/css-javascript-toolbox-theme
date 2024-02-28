17fe2328f18ad688b5b970aedcbbf8865e4b3331
adictonator <adityabhaskarsharma@gmail.com>
2024-02-28 14:14:28 +0530

    minor footer changes

--- footer.php
+++ footer.php
@@ -11,7 +11,20 @@
 
 ?>
 
-<footer class="site-footer divide-y divide-neutral-100">
+<footer class="site-footer divide-y divide-neutral-100 mt-20 lg:py-14 lg:px-24">
+	<svg width="270" height="296" viewBox="0 0 270 296" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g opacity="0.2" filter="url(#filter0_f_381_326)">
+			<ellipse cx="123" cy="148" rx="97" ry="98" fill="#AFF7EC" fill-opacity="0.6" />
+		</g>
+		<defs>
+			<filter id="filter0_f_381_326" x="-23.3211" y="0.678898" width="292.642" height="294.642" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+				<feFlood flood-opacity="0" result="BackgroundImageFix" />
+				<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+				<feGaussianBlur stdDeviation="24.6606" result="effect1_foregroundBlur_381_326" />
+			</filter>
+		</defs>
+	</svg>
+
 	<div class="grid lg:grid-cols-5 lg:gap-x-6 container mx-auto">
 		<div>logo</div>
 		<div>footer menu</div>
@@ -20,15 +33,12 @@
 		<div>newsletter</div>
 	</div>
 
-	<div>
-		<a href="<?php echo esc_url(__(site_url(), 'cjt')); ?>">
-			<?php
-			/* translators: %s: CMS name, i.e. WordPress. */
-			printf(esc_html__('Copyright &copy; %s', 'cjt'), 'CSS & JavaScript Toolbox');
-			echo date('Y');
-			?>
-		</a>
-	</div>
+	<p class="text-sm text-center py-10">
+		<?php
+		/* translators: %s: CMS name, i.e. WordPress. */
+		printf(esc_html__('Copyright &copy; %d %s', 'cjt'), date('Y'), 'CSS & JavaScript Toolbox');
+		?>
+	</p>
 </footer>
 </div>
 
