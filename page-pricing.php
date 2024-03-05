caf5e0341729734b3a85cb956b724b1dbf655834
Nisarg <kavinisarg1@gmail.com>
2024-03-05 18:13:21 +0530

    Completed pricing section

---
+++ page-pricing.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Template Name: Pricing page
+ * Post Type: post, page, event
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package cjt
+ */
+
+get_header();
+?>
+
+<main id="app">
+
+    <?php
+    get_template_part('template-parts/sections/pricing/section', 'hero');
+    get_template_part('template-parts/sections/pricing/section', 'features');
+    get_template_part('template-parts/sections/pricing/section', 'compression');
+    ?>
+
+</main>
+
+<?php
+
+get_footer();
---
+++ template-parts/sections/pricing/section-hero.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package cjt
+ */
+?>
+
+
+<section class="py-24 relative">
+    <div class="mx-auto container text-center">
+        <h2 class="text-2xl font-bold md:text-5xl font-display">
+            Pricing
+        </h2>
+
+        <div class="grid grid-cols-1 sm:grid-cols-2 sm:items-stretch md:grid-cols-4 md:gap-8 rounded-lg bg-white mt-44">
+            <div class="p-10 divide-gray-200 space-y-5">
+                <div class="space-y-2.5 text-start">
+                    <div class="space-y-5">
+                        <h2>
+                            <strong class="text-3xl font-medium text-gray-900 sm:text-4xl"> 20$ </strong>
+                            <span class="text-sm font-medium text-gray-700">/month</span>
+                        </h2>
+                        <p class="text-3xl font-medium text-indigo-950">Intro</p>
+                    </div>
+                    <p class="text-gray-600">CJT PLUS premium WordPress plugin</p>
+                </div>
+
+
+                <ul class="space-y-2.5">
+                    <?php for ($x = 0; $x <= 5; $x++) { ?>
+                        <li class="flex items-center gap-2.5">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20C15.5228 20 20 15.5228 20 10C19.9936 4.47982 15.5202 0.00642897 10 0Z" fill="#5243C2" fill-opacity="0.103693" />
+                                <path d="M15.7741 6.83362L10.07 14.5745C9.93392 14.7549 9.73107 14.8732 9.50698 14.9027C9.28289 14.9321 9.05636 14.8703 8.8783 14.7311L4.80496 11.4745C4.44552 11.1868 4.38731 10.6622 4.67496 10.3028C4.96261 9.94334 5.48718 9.88514 5.84663 10.1728L9.2433 12.8903L14.4325 5.84778C14.6026 5.59244 14.8993 5.45127 15.2048 5.48032C15.5103 5.50936 15.7751 5.70393 15.8941 5.98676C16.013 6.2696 15.967 6.59494 15.7741 6.83362Z" fill="#005DA4" />
+                            </svg>
+
+                            <span class="text-gray-700">1 site install only</span>
+                        </li>
+                    <?php } ?>
+                </ul>
+                <button class="rounded-lg inline-flex px-6 text-xl font-semibold py-3.5 text-brand-blue bg-brand-blue/10">
+                    Explore Plugins Now
+                </button>
+            </div>
+            <div class="p-10 divide-gray-200 space-y-5">
+                <div class="space-y-2.5 text-start">
+                    <div class="space-y-5">
+                        <h2>
+                            <strong class="text-3xl font-medium text-gray-900 sm:text-4xl"> 20$ </strong>
+                            <span class="text-sm font-medium text-gray-700">/month</span>
+                        </h2>
+                        <p class="text-3xl font-medium text-indigo-950">Intro</p>
+                    </div>
+                    <p class="text-gray-600">CJT PLUS premium WordPress plugin</p>
+                </div>
+
+
+                <ul class="space-y-2.5">
+                    <?php for ($x = 0; $x <= 5; $x++) { ?>
+                        <li class="flex items-center gap-2.5">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20C15.5228 20 20 15.5228 20 10C19.9936 4.47982 15.5202 0.00642897 10 0Z" fill="#5243C2" fill-opacity="0.103693" />
+                                <path d="M15.7741 6.83362L10.07 14.5745C9.93392 14.7549 9.73107 14.8732 9.50698 14.9027C9.28289 14.9321 9.05636 14.8703 8.8783 14.7311L4.80496 11.4745C4.44552 11.1868 4.38731 10.6622 4.67496 10.3028C4.96261 9.94334 5.48718 9.88514 5.84663 10.1728L9.2433 12.8903L14.4325 5.84778C14.6026 5.59244 14.8993 5.45127 15.2048 5.48032C15.5103 5.50936 15.7751 5.70393 15.8941 5.98676C16.013 6.2696 15.967 6.59494 15.7741 6.83362Z" fill="#005DA4" />
+                            </svg>
+
+                            <span class="text-gray-700">1 site install only</span>
+                        </li>
+                    <?php } ?>
+                </ul>
+                <button class="rounded-lg inline-flex px-6 text-xl font-semibold py-3.5 text-brand-blue bg-brand-blue/10">
+                    Explore Plugins Now
+                </button>
+            </div>
+            <div class="p-10 divide-gray-200 rounded-3xl shadow-[0px_40px_30px_0px] shadow-brand-blue/25 relative -top-28 bg-brand-blue space-y-5">
+                <div class="flex justify-end">
+                    <div class="bg-white rounded-full px-4 py-2 text-xs text-brand-blue font-semibold">
+                        MOST POPULAR
+                    </div>
+                </div>
+                <div class="space-y-2.5 text-start">
+                    <div class="space-y-5">
+                        <h2 class="text-white">
+                            <strong class="text-3xl font-medium sm:text-4xl">50$</strong>
+                            <span class="text-sm font-medium">/month</span>
+                        </h2>
+                        <p class="text-3xl font-medium text-white">Grand</p>
+                    </div>
+                    <p class="text-white">CJT PLUS premium WordPress plugin</p>
+                </div>
+
+
+                <ul class="space-y-2.5">
+                    <?php for ($x = 0; $x <= 5; $x++) { ?>
+                        <li class="flex items-center gap-2.5">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20C15.5228 20 20 15.5228 20 10C19.9936 4.47982 15.5202 0.00642897 10 0Z" fill="#FFFFFF1A" />
+                                <path d="M15.7741 6.83362L10.07 14.5745C9.93392 14.7549 9.73107 14.8732 9.50698 14.9027C9.28289 14.9321 9.05636 14.8703 8.8783 14.7311L4.80496 11.4745C4.44552 11.1868 4.38731 10.6622 4.67496 10.3028C4.96261 9.94334 5.48718 9.88514 5.84663 10.1728L9.2433 12.8903L14.4325 5.84778C14.6026 5.59244 14.8993 5.45127 15.2048 5.48032C15.5103 5.50936 15.7751 5.70393 15.8941 5.98676C16.013 6.2696 15.967 6.59494 15.7741 6.83362Z" fill="#FFFFFF" />
+                            </svg>
+
+                            <span class="text-white">1 site install only</span>
+                        </li>
+                    <?php } ?>
+                </ul>
+                <button class="rounded-lg inline-flex px-6 text-xl font-semibold py-3.5 text-brand-blue bg-white">
+                    Explore Plugins Now
+                </button>
+            </div>
+            <div class="p-10 divide-gray-200 space-y-5">
+                <div class="space-y-2.5 text-start">
+                    <div class="space-y-5">
+                        <h2>
+                            <strong class="text-3xl font-medium text-gray-900 sm:text-4xl"> 20$ </strong>
+                            <span class="text-sm font-medium text-gray-700">/month</span>
+                        </h2>
+                        <p class="text-3xl font-medium text-indigo-950">Intro</p>
+                    </div>
+                    <p class="text-gray-600">CJT PLUS premium WordPress plugin</p>
+                </div>
+
+
+                <ul class="space-y-2.5">
+                    <?php for ($x = 0; $x <= 5; $x++) { ?>
+                        <li class="flex items-center gap-2.5">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20C15.5228 20 20 15.5228 20 10C19.9936 4.47982 15.5202 0.00642897 10 0Z" fill="#5243C2" fill-opacity="0.103693" />
+                                <path d="M15.7741 6.83362L10.07 14.5745C9.93392 14.7549 9.73107 14.8732 9.50698 14.9027C9.28289 14.9321 9.05636 14.8703 8.8783 14.7311L4.80496 11.4745C4.44552 11.1868 4.38731 10.6622 4.67496 10.3028C4.96261 9.94334 5.48718 9.88514 5.84663 10.1728L9.2433 12.8903L14.4325 5.84778C14.6026 5.59244 14.8993 5.45127 15.2048 5.48032C15.5103 5.50936 15.7751 5.70393 15.8941 5.98676C16.013 6.2696 15.967 6.59494 15.7741 6.83362Z" fill="#005DA4" />
+                            </svg>
+
+                            <span class="text-gray-700">1 site install only</span>
+                        </li>
+                    <?php } ?>
+                </ul>
+                <button class="rounded-lg inline-flex px-6 text-xl font-semibold py-3.5 text-brand-blue bg-brand-blue/10">
+                    Explore Plugins Now
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
