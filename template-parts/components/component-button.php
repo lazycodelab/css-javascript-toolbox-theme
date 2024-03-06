4c4e92328f1e0520af42b75fc3c93708057d25c8
Nisarg <kavinisarg1@gmail.com>
2024-03-06 12:39:25 +0530

    Major updates: Added SVGs to file folder accordingly and also removed large SVG codes

--- template-parts/components/component-button.php
+++ template-parts/components/component-button.php
@@ -1,3 +1,3 @@
-<a href="<?php echo $args['url']; ?>" class="bg-brand-blue rounded mt-10 border border-brand-blue/70 px-8 py-3 shadow-md text-white text-xl inline-flex justify-center items-center hover:bg-brand-blue/90 duration-300 hover:shadow-xl">
-	<?php echo $args['content']; ?>
+<a href="<?= $args['url']; ?>" class="bg-brand-blue rounded mt-10 border border-brand-blue/70 px-8 py-3 shadow-md text-white text-xl inline-flex justify-center items-center hover:bg-brand-blue/90 duration-300 hover:shadow-xl">
+	<?= $args['content']; ?>
 </a>
