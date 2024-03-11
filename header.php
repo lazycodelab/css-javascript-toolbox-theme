17cafd511c987a5bd7fd085250f18ecfce1d8e65
adictonator <adityabhaskarsharma@gmail.com>
2024-03-11 22:47:04 +0530

    some minor tweaks to landing page header

--- header.php
+++ header.php
@@ -14,77 +14,60 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 
-<head>
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-	<?php wp_head(); ?>
-</head>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+		<?php wp_head(); ?>
+	</head>
 
-<body <?php body_class(); ?>>
-	<?php wp_body_open(); ?>
-	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#primary">
-			<?php esc_html_e('Skip to content', 'cjt'); ?>
-		</a>
-		<div class="bg-brand-blue px-5 lg:px-0 py-5 text-white">
-			<div class="flex lg:flex-row flex-col lg:text-start text-center gap-y-2 justify-between container mx-auto">
-				<p class="text-base font-medium">Get 20% Discounts! offer in 2 days</p>
-				<div class="divide-x-2 inline-flex items-center justify-center">
-					<a href="tel:+23561236456" class="text-base inline-flex items-center font-medium px-5 gap-2.5">
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-							<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
-						</svg>
-						+23561236456
-					</a>
-					<a href="tel:+23561236456" class="text-base inline-flex items-center font-medium px-5 gap-2.5">
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-							<path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-						</svg>
-						Support
-					</a>
-				</div>
-			</div>
-		</div>
-		<header class="bg-white">
-			<div class="mx-auto container p-5">
-				<div class="flex items-center justify-between">
-					<div class="flex-1 md:flex md:items-center md:gap-12">
-						<a class="block" href="/">
-							<img src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/logo.svg" />
-						</a>
-					</div>
+	<body <?php body_class(); ?>>
+		<?php wp_body_open(); ?>
+		<div id="page" class="site">
+			<a class="skip-link screen-reader-text" href="#primary">
+				<?php esc_html_e('Skip to content', 'cjt'); ?>
+			</a>
 
-					<div class="md:flex md:items-center md:gap-12">
-						<nav aria-label="Global" class="hidden md:block">
-							<?php
-							wp_nav_menu(array(
-								'menu_class'           => 'menu',
-								'menu_id'              => 'nav-menu',
-							));
-							?>
-						</nav>
+			<?php get_template_part('template-parts/components/component', 'announcement-bar'); ?>
+			<header class="bg-white">
+				<div class="mx-auto container p-5">
+					<div class="flex items-center justify-between">
+						<div class="flex-1 md:flex md:items-center md:gap-12">
+							<a class="block" href="/">
+								<img src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/logo.svg" />
+							</a>
+						</div>
 
-						<div class="flex items-center gap-4">
-							<div class="sm:flex sm:gap-4">
-								<a class="rounded-md bg-brand-blue lg:px-6 px-3 lg:text-lg text-sm lg:py-3.5 py-2 font-medium text-white" href="#">
-									Get CJT PLUS
-								</a>
-							</div>
+						<div class="md:flex md:items-center md:gap-12">
+							<nav aria-label="Global" class="hidden md:block">
+								<?php
+								wp_nav_menu(array(
+									'menu_class' => 'menu',
+									'menu_id'    => 'nav-menu',
+								));
+								?>
+							</nav>
 
-							<div class="block md:hidden">
-								<button class="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
-									<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-										<path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-									</svg>
-								</button>
+							<div class="flex items-center gap-4">
+								<div class="sm:flex sm:gap-4">
+									<a class="rounded-md bg-brand-blue lg:px-6 px-3 lg:text-lg text-sm lg:py-3.5 py-2 font-medium text-white" href="#">
+										Get CJT PLUS
+									</a>
+								</div>
+
+								<div class="block md:hidden">
+									<button class="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+										<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+											<path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+										</svg>
+									</button>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</header>
+			</header>
--- template-parts/sections/section-hero.php
+++ template-parts/sections/section-hero.php
@@ -102,15 +102,17 @@
 		</div>
 	</div>
 
-	<div class="absolute bottom-0 w-full right-0 z-10">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-			<path fill="#3AAAFF" fill-opacity="0.35" d="M0,192L40,213.3C80,235,160,277,240,250.7C320,224,400,128,480,128C560,128,640,224,720,245.3C800,267,880,213,960,160C1040,107,1120,53,1200,32C1280,11,1360,21,1400,26.7L1440,32L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
-		</svg>
-	</div>
-	<div class="absolute bottom-0 w-full left-0">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-			<path fill="#005DA4" fill-opacity="1" d="M0,32L34.3,32C68.6,32,137,32,206,58.7C274.3,85,343,139,411,176C480,213,549,235,617,224C685.7,213,754,171,823,149.3C891.4,128,960,128,1029,144C1097.1,160,1166,192,1234,176C1302.9,160,1371,96,1406,64L1440,32L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
-		</svg>
+	<div class="absolute bottom-0 w-full -z-[1]">
+		<div class="absolute bottom-0 w-full right-0 z-10">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+				<path fill="#3AAAFF" fill-opacity="0.35" d="M0,192L40,213.3C80,235,160,277,240,250.7C320,224,400,128,480,128C560,128,640,224,720,245.3C800,267,880,213,960,160C1040,107,1120,53,1200,32C1280,11,1360,21,1400,26.7L1440,32L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
+			</svg>
+		</div>
+		<div class="absolute bottom-0 w-full left-0">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+				<path fill="#005DA4" fill-opacity="1" d="M0,32L34.3,32C68.6,32,137,32,206,58.7C274.3,85,343,139,411,176C480,213,549,235,617,224C685.7,213,754,171,823,149.3C891.4,128,960,128,1029,144C1097.1,160,1166,192,1234,176C1302.9,160,1371,96,1406,64L1440,32L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+			</svg>
+		</div>
 	</div>
 	<!-- Blue blob start -->
 	<div class="absolute left-0 top-0">
@@ -128,6 +130,26 @@
 		</svg>
 	</div>
 	<!-- Blue blob end -->
+
+	<div class="size-5 rounded-md bg-blue-200/50 absolute rotate-6 left-1/2 top-20"></div>
+	<div class="size-5 rounded-md bg-amber-200/50 absolute -rotate-6 right-1/3 bottom-1/2"></div>
+	<div class="size-5 rounded-md bg-lime-200/50 absolute rotate-3 left-1/3 top-1/3 -z-[1]"></div>
+	<div class="size-5 rounded-md bg-blue-200/50 absolute left-96 top-2/4 -z-[1]"></div>
+
+	<div class="absolute bottom-0 -z-[1]">
+		<svg viewBox="0 0 330 628" fill="none" class="fill-amber-200" xmlns="http://www.w3.org/2000/svg">
+			<g opacity="0.5" filter="url(#filter0_f_316_11644)">
+				<circle cx="16" cy="314" r="236" fill="currentColor" fill-opacity="0.3" />
+			</g>
+			<defs>
+				<filter id="filter0_f_316_11644" x="-297.25" y="0.749588" width="626.501" height="626.501" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+					<feFlood flood-opacity="0" result="BackgroundImageFix" />
+					<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+					<feGaussianBlur stdDeviation="38.6252" result="effect1_foregroundBlur_316_11644" />
+				</filter>
+			</defs>
+		</svg>
+	</div>
 	<!-- Youtube player start -->
 	<div class="absolute -bottom-[40%] z-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 xl:block hidden">
 		<div class="bg-white p-2.5 shadow-xl">
