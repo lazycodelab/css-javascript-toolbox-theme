f025cac7804db3dc2184a75745c1c9306619610b
adictonator <adityabhaskarsharma@gmail.com>
2024-05-13 20:58:17 +0530

    sr only fix

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 	<body <?php body_class(); ?>>
 		<?php wp_body_open(); ?>
 		<div id="page" class="site">
-			<a class="skip-link screen-reader-text" href="#primary">
+			<a class="sr-only" href="#primary">
 				<?php esc_html_e('Skip to content', 'cjt'); ?>
 			</a>
 
