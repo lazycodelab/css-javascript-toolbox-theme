a8cadb489f39ed04f8abe7f42b6bf7df79d530f2
adictonator <adityabhaskarsharma@gmail.com>
2024-05-31 20:52:00 +0530

    more copy update

--- template-parts/sections/section-cards.php
+++ template-parts/sections/section-cards.php
@@ -22,70 +22,114 @@
 </section>-->
 
 <section class="relative mx-auto max-w-7xl pt-10 pb-10 lg:pb-14 px-4">
-	<h2 class="text-2xl md:max-w-2xl mx-auto font-bold md:text-5xl font-display text-center md:leading-tight">
-		CSS & JavaScript Toolbox is <span class="text-brand-blue">used</span> for
+	<h2
+		class="text-2xl md:max-w-2xl mx-auto font-bold md:text-5xl font-display text-center md:leading-tight">
+		CSS & JavaScript Toolbox is <span class="text-brand-blue">used</span>
+		for
 	</h2>
-	<div class="grid grid-cols-1 gap-5 lg:grid-cols-2 lg:grid-rows-2 lg:gap-x-10 lg:gap-y-14 lg:px-0 px-5 items-start mt-10 lg:mt-24">
-		<div class="grid justify-items-center justify-center md:text-start text-center items-center lg:space-y-5 space-y-2.5">
+	<div
+		class="grid grid-cols-1 gap-5 lg:grid-cols-3 lg:gap-x-10 lg:gap-y-14 lg:px-0 px-5 items-start mt-10 lg:mt-24">
+		<div
+			class="grid justify-items-center justify-center md:text-start text-center items-center lg:space-y-5 space-y-2.5">
 			<div class="space-y-5 flex flex-col items-center">
-				<svg xmlns="http://www.w3.org/2000/svg" class="size-16 p-2.5 bg-cyan-100 text-cyan-600 rounded" viewBox="0 0 24 24" fill="none">
-					<path d="M9.00002 13C8.34002 13.33 7.79002 13.82 7.38002 14.43C7.15002 14.78 7.15002 15.22 7.38002 15.57C7.79002 16.18 8.34002 16.67 9.00002 17" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-					<path d="M15.21 13C15.87 13.33 16.42 13.82 16.83 14.43C17.06 14.78 17.06 15.22 16.83 15.57C16.42 16.18 15.87 16.67 15.21 17" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-					<path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-					<path d="M2.22998 8.01L21.45 8" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+				<svg xmlns="http://www.w3.org/2000/svg"
+					class="size-16 p-2.5 bg-cyan-100 text-cyan-600 rounded"
+					viewBox="0 0 24 24" fill="none">
+					<path
+						d="M9.00002 13C8.34002 13.33 7.79002 13.82 7.38002 14.43C7.15002 14.78 7.15002 15.22 7.38002 15.57C7.79002 16.18 8.34002 16.67 9.00002 17"
+						stroke="currentColor" stroke-width="1.5"
+						stroke-linecap="round" stroke-linejoin="round" />
+					<path
+						d="M15.21 13C15.87 13.33 16.42 13.82 16.83 14.43C17.06 14.78 17.06 15.22 16.83 15.57C16.42 16.18 15.87 16.67 15.21 17"
+						stroke="currentColor" stroke-width="1.5"
+						stroke-linecap="round" stroke-linejoin="round" />
+					<path
+						d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
+						stroke="currentColor" stroke-width="1.5"
+						stroke-linecap="round" stroke-linejoin="round" />
+					<path d="M2.22998 8.01L21.45 8" stroke="currentColor"
+						stroke-width="1.5" stroke-linecap="round"
+						stroke-linejoin="round" />
 				</svg>
-				<h3 class="text-xl font-semibold text-gray-900 md:text-2xl">Making CSS front-end changes</h3>
+				<h3 class="text-xl font-semibold text-gray-900 md:text-2xl">
+					Making CSS front-end changes</h3>
 			</div>
 			<p class="text-neutral-500 text-center">
-				No need to modify theme files. Just create a CSS code block.
+				No need to modify theme files or work with tedious built-in
+				theme options. Just create a CSS code block.
 			</p>
 		</div>
-		<div class="grid justify-items-center justify-center md:text-start text-center items-center lg:space-y-5 space-y-2.5">
+		<div
+			class="grid justify-items-center justify-center md:text-start text-center items-center lg:space-y-5 space-y-2.5">
 			<div class="space-y-5 flex flex-col items-center">
-				<svg xmlns="http://www.w3.org/2000/svg" class="size-16 p-2.5 rounded bg-red-100 text-red-600" viewBox="0 0 24 24" fill="none">
-					<path d="M22 5.14999V7.85001C22 10.1 21.1 11 18.85 11H16.15C13.9 11 13 10.1 13 7.85001V5.14999C13 2.89999 13.9 2 16.15 2H18.85C21.1 2 22 2.89999 22 5.14999Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-					<path d="M11 16.15V18.85C11 21.1 10.1 22 7.85 22H5.15C2.9 22 2 21.1 2 18.85V16.15C2 13.9 2.9 13 5.15 13H7.85C10.1 13 11 13.9 11 16.15Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-					<path d="M17.38 11.0001V13.6201C17.38 16.3101 16.31 17.3801 13.62 17.3801H11V16.1501C11 13.9001 10.1 13.0001 7.85 13.0001H6.62V10.3801C6.62 7.69013 7.69 6.62012 10.38 6.62012H13V7.85013C13 10.1001 13.9 11.0001 16.15 11.0001H17.38Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+				<svg xmlns="http://www.w3.org/2000/svg"
+					class="size-16 p-2.5 rounded bg-red-100 text-red-600"
+					viewBox="0 0 24 24" fill="none">
+					<path
+						d="M22 5.14999V7.85001C22 10.1 21.1 11 18.85 11H16.15C13.9 11 13 10.1 13 7.85001V5.14999C13 2.89999 13.9 2 16.15 2H18.85C21.1 2 22 2.89999 22 5.14999Z"
+						stroke="currentColor" stroke-width="1.5"
+						stroke-linecap="round" stroke-linejoin="round" />
+					<path
+						d="M11 16.15V18.85C11 21.1 10.1 22 7.85 22H5.15C2.9 22 2 21.1 2 18.85V16.15C2 13.9 2.9 13 5.15 13H7.85C10.1 13 11 13.9 11 16.15Z"
+						stroke="currentColor" stroke-width="1.5"
+						stroke-linecap="round" stroke-linejoin="round" />
+					<path
+						d="M17.38 11.0001V13.6201C17.38 16.3101 16.31 17.3801 13.62 17.3801H11V16.1501C11 13.9001 10.1 13.0001 7.85 13.0001H6.62V10.3801C6.62 7.69013 7.69 6.62012 10.38 6.62012H13V7.85013C13 10.1001 13.9 11.0001 16.15 11.0001H17.38Z"
+						stroke="currentColor" stroke-width="1.5"
+						stroke-linecap="round" stroke-linejoin="round" />
 				</svg>
 
-				<h3 class="text-xl font-semibold text-gray-900 md:text-2xl">Adding site functionality</h3>
+				<h3 class="text-xl font-semibold text-gray-900 md:text-2xl">
+					Adding site functionality</h3>
 			</div>
 			<p class="text-neutral-500 text-center">
-				Stop adding plugins for simple tasks. They can slow down your site. Create a JavaScript or PHP code block instead.
+				Stop adding plugins for simple tasks as they can slow down your
+				site. Create a JavaScript or PHP code block instead.
 			</p>
 		</div>
-		<div class="grid justify-items-center justify-center md:text-start text-center items-center lg:space-y-5 space-y-2.5">
+		<div
+			class="grid justify-items-center justify-center md:text-start text-center items-center lg:space-y-5 space-y-2.5">
 			<div class="space-y-5 flex flex-col items-center">
-				<svg xmlns="http://www.w3.org/2000/svg" class="size-16 p-2.5 rounded bg-sky-100 text-sky-600" viewBox="0 0 24 24" fill="none">
-					<path d="M12.1401 15.0701V13.11C12.1401 10.59 14.1801 8.54004 16.7101 8.54004H18.6701" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-					<path d="M5.62012 8.55005H7.58014C10.1001 8.55005 12.1501 10.59 12.1501 13.12V13.7701V17.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-					<path d="M7.14008 6.75L5.34009 8.55L7.14008 10.35" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-					<path d="M16.8601 6.75L18.6601 8.55L16.8601 10.35" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-					<path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+				<svg xmlns="http://www.w3.org/2000/svg"
+					class="size-16 p-2.5 rounded bg-sky-100 text-sky-600"
+					viewBox="0 0 24 24" fill="none">
+					<path
+						d="M12.1401 15.0701V13.11C12.1401 10.59 14.1801 8.54004 16.7101 8.54004H18.6701"
+						stroke="currentColor" stroke-width="1.5"
+						stroke-linecap="round" stroke-linejoin="round" />
+					<path
+						d="M5.62012 8.55005H7.58014C10.1001 8.55005 12.1501 10.59 12.1501 13.12V13.7701V17.25"
+						stroke="currentColor" stroke-width="1.5"
+						stroke-linecap="round" stroke-linejoin="round" />
+					<path d="M7.14008 6.75L5.34009 8.55L7.14008 10.35"
+						stroke="currentColor" stroke-width="1.5"
+						stroke-linecap="round" stroke-linejoin="round" />
+					<path d="M16.8601 6.75L18.6601 8.55L16.8601 10.35"
+						stroke="currentColor" stroke-width="1.5"
+						stroke-linecap="round" stroke-linejoin="round" />
+					<path
+						d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
+						stroke="currentColor" stroke-width="1.5"
+						stroke-linecap="round" stroke-linejoin="round" />
 				</svg>
-				<h3 class="text-xl font-semibold text-gray-900 md:text-2xl">Third-party scripts</h3>
+				<h3 class="text-xl font-semibold text-gray-900 md:text-2xl">
+					Third-party scripts</h3>
 			</div>
 			<p class="text-neutral-500 text-center">
-				Code blocks are perfect for adding scripts such as Google Analytics and chat widgets to your website.
-			</p>
-		</div>
-		<div class="grid justify-items-center justify-center md:text-start text-center items-center lg:space-y-5 space-y-2.5">
-			<div class="space-y-5 flex flex-col items-center">
-				<svg xmlns="http://www.w3.org/2000/svg" class="size-16 p-2.5 bg-green-100 rounded text-green-600" viewBox="0 0 24 24" fill="none">
-					<path d="M2 2V19C2 20.66 3.34 22 5 22H22" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-					<path d="M5 17L9.59 11.64C10.35 10.76 11.7 10.7 12.52 11.53L13.47 12.48C14.29 13.3 15.64 13.25 16.4 12.37L21 7" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-				</svg>
-				<h3 class="text-xl font-semibold text-gray-900 md:text-2xl">Fast site development</h3>
-			</div>
-			<p class="text-neutral-500 text-center">
-				FTP? No thanks! Create code blocks for all your code snippets and scripts and view them all in one dashboard.
+				Code blocks are perfect for adding scripts such as Google
+				Analytics and chat widgets to your website.
 			</p>
 		</div>
 	</div>
 
 	<!-- Youtube player start -->
-	<div class="z-20 xl:flex items-center mx-auto justify-center hidden bg-white p-1 shadow-xl mt-10 lg:mt-24 max-w-lg h-full">
-		<iframe class="w-full" height="300" src="https://www.youtube.com/embed/vYAKePVgJqE?si=n0rU6Uv8s_6pvhnF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+	<div
+		class="z-20 xl:flex items-center mx-auto justify-center hidden bg-white p-1 shadow-xl mt-10 lg:mt-24 max-w-lg h-full">
+		<iframe class="w-full" height="300"
+			src="https://www.youtube.com/embed/vYAKePVgJqE?si=n0rU6Uv8s_6pvhnF"
+			title="YouTube video player" frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			allowfullscreen></iframe>
 	</div>
 	<!-- Youtube player end -->
 </section>
--- template-parts/sections/section-reasons.php
+++ template-parts/sections/section-reasons.php
@@ -11,126 +11,221 @@
 ?>
 
 <section class="md:pt-24 py-10 relative overflow-hidden">
-	<div class="mx-auto max-w-7xl md:space-y-10 space-y-14 xl:px-0 px-5">
-		<div class="relative">
-			<h2 class="text-2xl font-bold md:text-5xl font-display md:max-w-4xl max-w-72 mx-auto text-center">
-				<span class="text-brand-blue">Top reasons</span> why people choose CSS & JavaScript Toolbox
+	<div class="mx-auto max-w-7xl space-y-10 md:space-y-24 xl:px-0 px-5">
+		<div class="relative mb-24">
+			<h2
+				class="text-2xl font-bold md:text-5xl font-display md:max-w-4xl max-w-72 mx-auto text-center">
+				Explore the <span class="text-brand-blue">premium</span> <br />
+				CSS & JavaScript Toolbox PLUS
 			</h2>
 			<?php get_template_part('template-parts/components/component', 'blob', ['class' => 'top-0 md:left-[55%] left-20 bg-cyan-400/30 md:blur-3xl blur-xl md:size-[200px] size-[120px] -z-[1]']); ?>
 			<?php get_template_part('template-parts/components/component', 'blob', ['class' => 'md:-top-10 top-8 md:left-[65%] left-52 bg-amber-400/40 md:blur-3xl blur-2xl md:size-[130px] size-[80px] -z-[1]']); ?>
 		</div>
 		<div class="grid grid-cols-1 gap-5 lg:grid-cols-2">
 			<div class="size-full md:mx-auto relative">
-				<img src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-1.svg" loading="lazy" />
+				<img src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-1.svg"
+					loading="lazy" />
 				<div class="absolute right-0 top-1/4 md:px-0 px-3">
-					<img class="md:w-3/4 w-full ml-auto" src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-1.png" />
+					<img class="md:w-3/4 w-full ml-auto"
+						src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-1.png" />
 				</div>
 			</div>
 			<div class="grid place-content-center md:gap-y-5 gap-y-2.5">
-				<h2 class="font-display font-bold md:text-4xl tracking-[2px] text-xl">Fast and simple to use</h2>
-				<div class="inline-flex items-start justify-center gap-2.5">
-					<div>
+				<h2
+					class="font-display font-bold md:text-4xl tracking-[2px] text-xl">
+					PREMIUM Editor Tools</h2>
+				<ul class="items-start justify-center space-y-3 md:space-y-5">
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
 						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
-					</div>
-					<span class="md:text-base text-sm text-neutral-500 font-normal">
-						Over 10 years of user feedback and testimonials (add link to testimonials page) has guided us to develop a code snippet and script injection plugin that is both fast and simple to use.
-					</span>
-				</div>
+						Full-width and full-screen mode for distraction-free
+						coding.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						Beautify your code to make it perfectly formatted for
+						code readability.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						Minify your code by removing white space for code
+						optimisation and faster website performance.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						Use the built-in Code Auto Completion (CAC) feature that
+						allows you to add code into the editor automatically.
+						The CAC utilises a code syntax dictionary so you can
+						quickly auto-complete CSS, JavaScript, PHP, and HTML
+						code syntax or embed code snippets. There are stacks of
+						code snippets for you to choose from and it even
+						supports your own local variables.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						Choose from over 32 editor themes for changing the
+						editor background colour, syntax highlighting colour, or
+						adjust the font-size to just the way you like it.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						There are many useful editor tools that you can use such
+						as: undo, redo, find and replace, go to line, go to next
+						error, and much more.
+					</li>
+				</ul>
 			</div>
 		</div>
 		<div class="grid grid-cols-1 gap-5 lg:grid-cols-2">
 			<div class="grid place-content-center md:gap-y-5 gap-y-2.5">
-				<h2 class="font-display font-bold md:text-4xl tracking-[2px] text-xl">Finally, a code editor that does it all</h2>
-				<div class="inline-flex items-start justify-center gap-2.5">
-					<div>
+				<h2
+					class="font-display font-bold md:text-4xl tracking-[2px] text-xl">
+					PREMIUM Location Editor</h2>
+				<ul class="items-start justify-center space-y-3 md:space-y-5">
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
 						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
-					</div>
-					<span class="md:text-base text-sm text-neutral-500 font-normal">
-						Our code editor is more than just a plain text field. It features tools you would expect in a desktop (IDE) editor such as code auto completion, syntax highlighting and error checking, and code revisions and backups. Supports CSS, JavaScript, PHP, HTML, and plain text.
-					</span>
-				</div>
+						Assign code blocks to run on WordPress common website
+						locations such as: entire website, all pages, all posts,
+						recent posts, blog index, all categories, search pages,
+						all archives, tag archives, author archives, website
+						backend, 404 errors, and more.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						Assign code blocks to run on specific website URLs or
+						via Regex (Regular Expressions).
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						We include a powerful invert feature that allows you to
+						run your code everywhere OTHER than what you selected in
+						the location editor.
+					</li>
+				</ul>
 			</div>
 
 			<div class="w-full h-full md:mx-auto relative md:order-1 -order-1">
-				<img src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-2.svg" loading="lazy" />
+				<img src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-2.svg"
+					loading="lazy" />
 				<div class="absolute right-0 md:top-1/4 top-5 md:px-0 px-3">
-					<img class="md:w-3/4 w-full ml-auto" src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-2.png" />
+					<img class="md:w-3/4 w-full ml-auto"
+						src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-2.png" />
 				</div>
 			</div>
 		</div>
 		<div class="grid grid-cols-1 gap-5 lg:grid-cols-2">
 			<div class="w-full h-full mx-auto relative">
-				<img src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-3.svg" loading="lazy" />
+				<img src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-3.svg"
+					loading="lazy" />
 				<div class="absolute left-0 top-1/4 md:px-0 px-3">
-					<img class="md:w-3/4 w-full ml-auto" src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-2.png" />
+					<img class="md:w-3/4 w-full ml-auto"
+						src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-2.png" />
 				</div>
 			</div>
 			<div class="grid place-content-center md:gap-y-5 gap-y-2.5">
-				<h2 class="font-display font-bold md:text-4xl tracking-[2px] text-xl">Choose exactly where to run your code</h2>
-				<div class="inline-flex items-start justify-center gap-2.5">
-					<div>
+				<h2
+					class="font-display font-bold md:text-4xl tracking-[2px] text-xl">
+					PREMIUM Additional Locations</h2>
+				<ul class="items-start justify-center space-y-3 md:space-y-5">
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
 						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
-					</div>
-					<span class="md:text-base text-sm text-neutral-500 font-normal">
-						Our location editor is like a switchboard that allows you to 'connect' your code to specific sections of your website. For example, you can choose your contact page to run a chat widget; or choose your entire website to run a Google Analytics script.
-					</span>
-				</div>
+						Assign code blocks to run on WordPress post tags.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						Assign code blocks to run on WordPress pages, posts or
+						custom posts via the shortcode embed feature.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						Assign code blocks to run on WordPress widgets. Great
+						for adding code or textual content to your website
+						sidebar or footer.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						Add a single metabox code block at the page or post edit
+						level. Great for one-off functions such as a page or
+						post redirect.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						For those that prefer to work with WordPress Gutenberg
+						blocks. Simply use our Gutenberg shortcode embed
+						feature.
+					</li>
+				</ul>
 			</div>
 		</div>
 		<div class="grid grid-cols-1 gap-5 lg:grid-cols-2">
 			<div class="grid place-content-center md:gap-y-5 gap-y-2.5">
-				<h2 class="font-display font-bold md:text-4xl tracking-[2px] text-xl">Built with security first</h2>
-				<div class="inline-flex items-start justify-center gap-2.5">
-					<div>
+				<h2
+					class="font-display font-bold md:text-4xl tracking-[2px] text-xl">
+					PREMIUM Code Block Toolbar</h2>
+				<ul class="items-start justify-center space-y-3 md:space-y-5">
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
 						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
-					</div>
-					<span class="md:text-base text-sm text-neutral-500 font-normal">
-						Add custom code safely - even PHP. Built with security in mind to STOP the hackers! You can also rest assured that all of your code blocks are safely stored in the database and your code can be recovered if required.
-					</span>
-				</div>
+						Code block information panel showing useful details such
+						as: code block ID and name, author, and when the block
+						was created and last modified.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						In the free version, you can choose to run your code in
+						the header or footer of the page. The premium version
+						gives you 8 extra hook locations such as: body open,
+						before or after post content, before or after post
+						excerpt, before or after posts list, and before post.
+						These additional hooks can be useful for things like
+						adding advertising banners just before blog posts.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						Assign code blocks to run on WordPress widgets. Great
+						for adding code or textual content to your website
+						sidebar or footer.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						Load code from an external URL link and the code will be
+						copied and embedded to your code block automatically.
+						This can be useful when adding code from GitHub, which
+						you can do by clicking into the script and copying the
+						link from the Raw button.
+					</li>
+					<li
+						class="md:text-base inline-flex gap-x-2.5 text-sm text-neutral-500 font-normal">
+						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
+						Don't lose your best code revision! Our code block
+						revisions system lists all saved revisions with
+						date/time stamp and author. This allows you to restore
+						any previously saved version.
+					</li>
+				</ul>
 			</div>
 			<div class="w-full h-full mx-auto relative md:order-1 -order-1">
-				<img src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-4.svg" loading="lazy" />
+				<img src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-4.svg"
+					loading="lazy" />
 				<div class="absolute right-0 md:top-1/3 top-4 md:px-0 px-3">
-					<img class="md:w-3/4 w-full mx-auto" src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-2.png" />
-				</div>
-			</div>
-		</div>
-		<div class="grid grid-cols-1 gap-5 lg:grid-cols-2">
-			<div class="w-full h-full mx-auto relative">
-				<img src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-5.svg" loading="lazy" />
-				<div class="absolute right-0 top-1/3 md:px-0 px-3">
-					<img class="md:w-3/4 w-full ml-auto" src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-2.png" />
-				</div>
-			</div>
-			<div class="grid place-content-center md:gap-y-5 gap-y-2.5">
-				<h2 class="font-display font-bold md:text-4xl tracking-[2px] text-xl">Robust and powerful</h2>
-				<div class="inline-flex items-start justify-center gap-2.5">
-					<div>
-						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
-					</div>
-					<span class="md:text-base text-sm text-neutral-500 font-normal">
-						Create as many code blocks as needed and see all your code and location outputs in the one dashboard. Don't worry, this solution has been tested and optimized to handle 100's of code blocks and thousands of locations (e.g. WordPress Pages or Posts).
-					</span>
-				</div>
-			</div>
-		</div>
-		<div class="grid grid-cols-1 gap-5 lg:grid-cols-2">
-			<div class="grid place-content-center md:gap-y-5 gap-y-2.5">
-				<h2 class="font-display font-bold md:text-4xl tracking-[2px] text-xl">Backed by a 7-day money-back guarantee</h2>
-				<div class="inline-flex items-start justify-center gap-2.5">
-					<div>
-						<?php get_template_part('template-parts/components/component', 'check-mark'); ?>
-					</div>
-					<span class="md:text-base text-sm text-neutral-500 font-normal">
-						We truly believe that our code snippet and script plugin will do exactly what you are wanting, however if you are not 100% satisfied with our solution, then you are entitled to receive a 100% refund within 7 days from your purchase.
-					</span>
-				</div>
-			</div>
-			<div class="w-full h-full mx-auto relative md:order-1 -order-1">
-				<img src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-4.svg" loading="lazy" />
-				<div class="absolute right-0 md:top-1/3 top-4 md:px-0 px-3">
-					<img class="md:w-3/4 w-full mx-auto" src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-2.png" />
+					<img class="md:w-3/4 w-full mx-auto"
+						src="<?= esc_url(get_template_directory_uri()); ?>/assets/images/background/vector-2.png" />
 				</div>
 			</div>
 		</div>
